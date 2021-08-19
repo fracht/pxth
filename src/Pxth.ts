f17faaf1ed9a9b7d305161d9c993a9e952d8198f
sirse <artiom.tretjakovas2@gmail.com>
2021-08-19 21:13:13 +0300

    Created pxth types

--- src/Pxth.ts
+++ src/Pxth.ts
@@ -1,3 +1,53 @@
-export type Pxth = Array<PxthSegment>;
+declare const NumberPxthToken: unique symbol;
 
-export type PxthSegment = string | symbol | number;
+type NumberPxth = {
+    _token: typeof NumberPxthToken;
+};
+
+declare const StringPxthToken: unique symbol;
+
+type StringPxth = {
+    _token: typeof StringPxthToken;
+};
+
+declare const BooleanPxthToken: unique symbol;
+
+type BooleanPxth = {
+    _token: typeof BooleanPxthToken;
+};
+
+declare const BigIntPxthToken: unique symbol;
+
+type BigIntPxth = {
+    _token: typeof BigIntPxthToken;
+};
+
+declare const UnknownPxthToken: unique symbol;
+
+type UnknownPxth = {
+    _token: typeof UnknownPxthToken;
+};
+
+declare const DatePxthToken: unique symbol;
+
+type DatePxth = {
+    _token: typeof DatePxthToken;
+};
+
+export type PrimitivePxth<V> = V extends string
+    ? StringPxth
+    : V extends number
+    ? NumberPxth
+    : V extends boolean
+    ? BooleanPxth
+    : V extends bigint
+    ? BigIntPxth
+    : UnknownPxth;
+
+export type Pxth<V> = V extends object
+    ? V extends Date
+        ? DatePxth
+        : {
+              [K in keyof V]: Pxth<V[K]>;
+          }
+    : PrimitivePxth<V>;
