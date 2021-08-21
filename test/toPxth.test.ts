c479a4d88ba2420da34a2e078c47da2a15ee3b12
sirse <artiom.tretjakovas2@gmail.com>
2021-08-21 15:31:52 +0300

    Removed tests

--- test/toPxth.test.ts
+++
@@ -1,9 +0,0 @@
-import { ROOT_PATH, toPxth } from '../src';
-
-describe('toPxth', () => {
-  it('should convert everything to pxth', () => {
-    expect(toPxth('hello.a.s')).toStrictEqual(['hello', 'a', 's']);
-    expect(toPxth(15)).toStrictEqual([15]);
-    expect(toPxth(ROOT_PATH)).toStrictEqual([]);
-  });
-});
