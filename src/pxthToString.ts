326e49f71b1352ee1cb973c6963fc4efe493f669
Artiom Tretjakovas <artiom.tretjakovas2@gmail.com>
2021-09-20 15:05:35 +0300

    Created parseSegmentsFromString function

--- src/pxthToString.ts
+++ src/pxthToString.ts
@@ -1,8 +1,9 @@
 import { getPxthSegments } from './getPxthSegments';
 import { Pxth } from './Pxth';
+import { RootPath, RootPathToken } from '.';
 
-export const pxthToString = <V>(pxth: Pxth<V>): string => {
+export const pxthToString = <V>(pxth: Pxth<V>): string | RootPath => {
     const segments = getPxthSegments(pxth);
 
-    return segments.join('.');
+    return segments.length === 0 ? RootPathToken : segments.join('.');
 };
