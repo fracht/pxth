4b3936e29b75aca6ae63f7a17c806ae033b701f1
Artiom Tretjakovas <44021713+ArtiomTr@users.noreply.github.com>
2021-11-05 16:38:32 +0200

    Fixed Pxth for arrays (#17)

--- src/Pxth.ts
+++ src/Pxth.ts
@@ -21,16 +21,25 @@ export type PrimitivePxth<V> = {
         : typeof UnknownPxthBrand;
 };
 
-export type ObjectPxth<V extends object> = {
-    [K in keyof V]: Pxth<V[K]>;
-} & {
+type WithoutMethodsKeys<V extends object> = {
+    [K in keyof V]: V[K] extends Function ? never : K;
+}[keyof V];
+
+type RecordOfPxth<V extends object> = {
+    [K in WithoutMethodsKeys<V>]: Pxth<V[K]>;
+};
+
+export type ObjectPxth<V extends object> = RecordOfPxth<V> & {
     [BrandKey]: typeof ObjectPxthBrand;
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type ArrayPxth<V extends Array<any>> = Array<Pxth<V[number]>> & {
-    [BrandKey]: typeof ArrayPxthBrand;
-};
+export type ArrayPxth<V extends Array<any>> = {
+    [K in number]: Pxth<V[number]>;
+} &
+    RecordOfPxth<Omit<Array<V[number]>, number>> & {
+        [BrandKey]: typeof ArrayPxthBrand;
+    };
 
 export type Pxth<V> = {
     [ObjectBrandKey]: 'brand';
