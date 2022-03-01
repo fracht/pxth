937bc84f1e186d19e9cc42ebb1fa38c29f28b34e
Aleksandras Šukelovič <alex.shukel@gmail.com>
2022-03-01 17:24:04 +0200

    Added samePxth tests

--- test/samePxth.test.ts
+++ test/samePxth.test.ts
@@ -1,13 +1,36 @@
 import { createPxth, samePxth } from '../src';
 
 describe('samePxth', () => {
-    it('should compare 2 paths correctly', () => {
+    it('should return true when 2 paths are the same', () => {
         expect(samePxth(createPxth([]), createPxth([]))).toBeTruthy();
         expect(
             samePxth(createPxth(['a', 'b']), createPxth(['a', 'b'])),
         ).toBeTruthy();
+        expect(samePxth(createPxth(['']), createPxth(['']))).toBeTruthy();
+        expect(
+            samePxth(createPxth(['', '.', '']), createPxth(['', '.', ''])),
+        ).toBeTruthy();
+        expect(
+            samePxth(createPxth(['$', '.', '!']), createPxth(['$', '.', '!'])),
+        ).toBeTruthy();
+    });
+
+    it('should return true when 2 paths are not the same', () => {
         expect(
             samePxth(createPxth(['a', 'b']), createPxth(['b', 'a'])),
+        ).toBeFalsy();
+        expect(samePxth(createPxth(['']), createPxth(['.']))).toBeFalsy();
+        expect(
+            samePxth(createPxth(['', '.', '']), createPxth(['', ',', ''])),
+        ).toBeFalsy();
+        expect(
+            samePxth(createPxth(['%', '.', '!']), createPxth(['!', '.', '%'])),
+        ).toBeFalsy();
+        expect(
+            samePxth(
+                createPxth(['', '.', '']),
+                createPxth(['', '.', '', 'asdf']),
+            ),
         ).toBeFalsy();
     });
 });
