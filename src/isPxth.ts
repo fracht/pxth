6b95c01b5d14d5879e298750607783295483e5a5
Artiom Tretjakovas <44021713+ArtiomTr@users.noreply.github.com>
2022-03-18 18:31:34 +0200

    Fixed isPxth function (#22)
    * Fixed isPxth function
    
    * Fixed eslint error

--- src/isPxth.ts
+++ src/isPxth.ts
@@ -1,5 +1,5 @@
 import { SegmentsToken } from './getPxthSegments';
 import { Pxth } from './Pxth';
 
-export const isPxth = (path: object): path is Pxth<unknown> =>
-    SegmentsToken in path;
+export const isPxth = (path: unknown): path is Pxth<unknown> =>
+    typeof path === 'object' && path !== null && SegmentsToken in path;
