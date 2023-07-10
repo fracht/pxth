fa4f9449c184284f6386e9cce7c6a06b7a384cb4
Aleksandras Sukelovic <alex.shukel@gmail.com>
2023-07-10 16:06:54 +0300

    Fixed jest config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,32 @@
-module.exports = {
-    collectCoverageFrom: ['./src/**'],
-    globals: {
-        'ts-jest': {
-            tsconfig: 'tsconfig.test.json',
-        },
+/**
+ * @type {import('ts-jest').JestConfigWithTsJest}
+ */
+const config = {
+    transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(js|cjs|jsx)$'"],
+    preset: 'ts-jest',
+    moduleFileExtensions: [
+        'ts',
+        'tsx',
+        'js',
+        'jsx',
+        'cjs',
+        'mjs',
+        'json',
+        'node',
+    ],
+    collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx,cjs,mjs}'],
+    testMatch: ['<rootDir>/**/*.(spec|test).{ts,tsx,js,jsx,cjs,mjs}'],
+    testEnvironmentOptions: {
+        url: 'http://localhost',
+    },
+    transform: {
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {
+                tsconfig: './tsconfig.test.json',
+            },
+        ],
     },
 };
+
+module.exports = config;
